237c148aa9bbf572803d55617fe24aa824df700f
edson <edsonfilho734@gmail.com>
2021-12-09 10:57:15 -0300

    EventControllerIT done

--- src/test/java/com/devsuperior/bds02/controllers/CityControllerIT.java
+++ src/test/java/com/devsuperior/bds02/controllers/CityControllerIT.java
@@ -1,19 +1,7 @@
 package com.devsuperior.bds02.controllers;
 
-import static org.mockito.Mockito.doThrow;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
-import com.devsuperior.bds02.entities.City;
-import com.devsuperior.bds02.entities.Event;
-import com.devsuperior.bds02.repositories.CityRepository;
-import com.devsuperior.bds02.repositories.EventRepository;
-import com.devsuperior.bds02.services.CityService;
-import com.devsuperior.bds02.services.exception.DatabaseException;
-import org.junit.jupiter.api.BeforeEach;
+import com.devsuperior.bds02.dto.CityDTO;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -24,10 +12,9 @@ import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.devsuperior.bds02.dto.CityDTO;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
-import java.time.LocalDate;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
